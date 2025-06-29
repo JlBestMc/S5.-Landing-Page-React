2a99a13abc242412ad5a45722ef73ef5ea6d9f73
JlBestMc <jlrcmt@gmail.com>
2025-06-29 04:53:25 +0200

    Feature anchor completed

---
+++ src/components/atoms/anchor.tsx
@@ -0,0 +1,14 @@
+interface AnchorProps {
+  ref: string;
+  styles?: string;
+  content: string;
+}
+
+export default function Anchor({ ref, styles = "", content }: AnchorProps) {
+    const baseClasses ="hover:text-red-400 tracking-widest text-sm font-semibold text-[#353541] cursor-[url('./assets/cursor.cur'),_auto] transition duration-300 ease-in-out";
+    return (
+    <a className={`${baseClasses} ${ref} ${styles}`}>
+      {content}
+    </a>
+  );
+}
