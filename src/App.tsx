aa5d8442af9e3862dcfe7cc40245fc3b3b11651d
JlBestMc <jlrcmt@gmail.com>
2025-06-30 00:29:39 +0200

    wip navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,24 @@
-import Button from "./components/atoms/Button";
-import Anchor from "./components/atoms/anchor";
-import Logo from "./components/atoms/Logo";
+
 import bookmark from "./assets/logo-bookmark.svg";
-import Burger from "./components/atoms/Burger";
-import { useState } from "react";
+import bookmarkMobile from "./assets/logo-bookmark-mobile.svg";
 import burgerIcon from "./assets/icon-hamburger.svg";
 import closeBurgerIcon from "./assets/icon-close.svg";
+import Navbar from "./components/molecules/Navbar";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
+
 
   return (
     <>
       <div>
-        <Button content="Get it on Chrome" variant="btnBlue" />
+        <Navbar 
+          burgerIconSrc={burgerIcon}
+          closeIconSrc={closeBurgerIcon}
+          logoSrc={bookmark}
+          logoMobileSrc={bookmarkMobile}>
+            
+          </Navbar>
       </div>
-      <div>
-        <Button content="LOGIN" variant="btnRed" />
-      </div>
-      <div>
-        <Button content="Get it on Firefox" variant="btnWhite" />
-      </div>
-      <div>
-        <Anchor content="CONTACT" ref="#contact" />
-      </div>
-      <div>
-        <Logo src={bookmark} />
-      </div>
-      <div>
-        <button onClick={() => setIsOpen(!isOpen)}>
-          <Burger
-            isOpen={isOpen}
-            burgerIconSrc={burgerIcon}
-            closeIconSrc={closeBurgerIcon}
-          ></Burger>
-        </button>
-        {/* Navbar móvil */}
-        {isOpen && (
-          <div className="bg-gray-800 text-white p-4 flex flex-col ">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-          </div>
-        )}
-      </div>
-      <p className="mt-4 text-black">
-        Estado: {isOpen ? "Abierto" : "Cerrado"}
-      </p>
     </>
   );
 }
---
+++ src/components/molecules/Navbar.tsx
@@ -0,0 +1,86 @@
+// components/organisms/Navbar.tsx
+import { useState } from "react";
+import Burger from "../atoms/Burger";
+import Logo from "../atoms/Logo";
+import Anchor from "../atoms/anchor";
+import Button from "../atoms/Button";
+
+interface NavbarProps {
+  burgerIconSrc: string;
+  closeIconSrc: string;
+  logoSrc: string;
+  logoMobileSrc: string;
+}
+
+export default function Navbar({
+  burgerIconSrc,
+  closeIconSrc,
+  logoSrc,
+  logoMobileSrc,
+}: NavbarProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentLogo, setCurrentLogo] = useState(logoSrc);
+  const [navBarStyles, setNavBarStyles] = useState("hidden");
+  const [containerPosition, setContainerPosition] = useState("");
+
+  const handleBurgerClick = () => {
+    const burgerOpen = !isOpen;
+    setIsOpen(burgerOpen);
+    setCurrentLogo(burgerOpen ? logoMobileSrc : logoSrc);
+
+    setNavBarStyles(() =>
+      burgerOpen
+        ? "w-full h-full top-0 left-0 fixed flex flex-col justify-center gap-10 z-10 bg-gray-700"
+        : "hidden"
+    );
+
+    setContainerPosition(() => (burgerOpen ? "relative" : ""));
+  };
+
+  return (
+      <div
+        className={`max-w-[1200px] w-[80%] md:w-full mx-auto flex justify-between items-center ${containerPosition}`}
+      >
+        <Logo src={currentLogo} styles="z-20" />
+
+        <button
+          onClick={handleBurgerClick}
+          className="md:hidden z-30 fixed top-15 right-6"
+        >
+          <Burger
+            isOpen={isOpen}
+            burgerIconSrc={burgerIconSrc}
+            closeIconSrc={closeIconSrc}
+          />
+        </button>
+
+        <nav
+          className={`${navBarStyles} md:grid md:grid-cols-4 gap-10 justify-items-center items-center text-gray-700 md:text-black uppercase font-medium tracking-widest md:static md:bg-transparent`}
+        >
+          <hr className="w-[80%] md:hidden opacity-30 text-white" />
+          <Anchor
+            ref="#features"
+            styles="text-white text-2xl md:text-gray-700 md:text-sm md:hover:text-red-400 md:py-3 md:px-6 md:hover:text-red-400 md:tracking-widest md:text-sm md:font-semibold md:text-[#353541] cursor-[url('./assets/cursor.cur'),_auto] transition duration-300 ease-in-out"
+          >
+            Features
+          </Anchor>
+          <hr className="w-[80%] md:hidden opacity-30 text-white" />
+          <Anchor
+            ref="#pricing"
+            styles="text-white text-2xl md:text-gray-700 md:text-sm md:hover:text-red-400 py-3 px-6"
+          >
+            Pricing
+          </Anchor>
+          <hr className="w-[80%] md:hidden opacity-30 text-white" />
+          <Anchor
+            ref="#contact"
+            styles="text-white text-2xl md:text-gray-700 md:text-sm md:hover:text-red-400 py-3 px-6"
+          >
+            Contact
+          </Anchor>
+          <hr className="w-[80%] md:hidden opacity-30 text-white" />
+          <Button content="LOGIN" styles="md:px-7 md:py-2 md:border-red-400 tracking-widest md:text-sm md:bg-red-400 md:hover:text-red-400 text-white bg-gray-700 border-white"></Button>
+        </nav>
+      </div>
+  );
+}
