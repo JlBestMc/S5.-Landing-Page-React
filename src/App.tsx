038e114ae3d7b0ab6407477191026087fcfb5686
JlBestMc <jlrcmt@gmail.com>
2025-06-29 03:26:06 +0200

    wip button component styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import "./App.css";
 import Button from "./components/atoms/Button";
-import Card from "./components/molecules/Card";
-import Hero from "./components/molecules/Hero";
 
 function App() {
-  const nombre1 = "Juan";
-  const nombre2 = "Luis";
+
   return (
     <>
-      <Button nombre={nombre1} edad={26} />
-      <Button nombre={nombre2} edad={55} />
-      <Card />
-      <Hero />
+      <Button content="Get it on Chrome" variant="blue" />
+      <Button content="Login" variant="red" />
+      <Button content="Get it on Firefox" variant="white" />
+
     </>
   );
 }
--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,8 +1,21 @@
-interface propsType {
-  styles: string;
+interface ButtonProps {
+  variant?: "blue" | "red" | "white";
+  styles?: string;
   content: string;
 }
-function Button({ styles, content }: propsType) {
-  return <div className={styles}>{content}</div>;
+
+export default function Button({ variant = "blue", styles = "", content }: ButtonProps) {
+  const baseClasses = "py-4 px-8 rounded font-semibold hover:bg-white border-2 rounded-md transition text-white";
+
+  const variants = {
+    blue: "px-5 bg-[#5465df] hover:text-[#5465df] border-[#5465df]",
+    red: "px-7 py-2 border-red-400 bg-red-400 hover:text-red-400",
+    white: "bg-white text-gray-550  border-white hover:border-gray-500 shadow-lg",
+  };
+
+  return (
+    <button className={`${baseClasses} ${variants[variant]} ${styles}`}>
+      {content}
+    </button>
+  );
 }
-export default Button;
