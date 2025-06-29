d61c003ec3f073f13b22652c999d35bf0a19d4a1
JlBestMc <jlrcmt@gmail.com>
2025-06-29 04:02:19 +0200

    Feature button finished

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,15 @@ function App() {
 
   return (
     <>
+    <div>
       <Button content="Get it on Chrome" variant="btnBlue" />
+      </div>
+      <div>
       <Button content="Login" variant="btnRed" />
+      </div>
+      <div>
       <Button content="Get it on Firefox" variant="btnWhite" />
+      </div>
 
     </>
   );
--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -4,13 +4,20 @@ interface ButtonProps {
   content: string;
 }
 
-export default function Button({ variant = "btnBlue", styles = "", content }: ButtonProps) {
-  const baseClasses = "py-4 px-8 rounded font-semibold hover:bg-white border-2 rounded-md transition cursor-[url('./assets/cursor.cur'),_auto]";
+export default function Button({
+  variant = "btnBlue",
+  styles = "",
+  content,
+}: ButtonProps) {
+  const baseClasses =
+    "font-semibold hover:bg-white border-2 rounded transition cursor-[url('./assets/cursor.cur'),_auto]";
 
   const variants = {
-    btnBlue: "px-5 bg-[#5465df] hover:text-[#5465df] border-[#5465df] text-white",
+    btnBlue:
+      "py-4 px-8 bg-[#5465df] hover:text-[#5465df] border-[#5465df] text-white",
     btnRed: "px-7 py-2 border-red-400 bg-red-400 hover:text-red-400 text-white",
-    btnWhite: "bg-white text-gray-600 border-white hover:border-gray-500 shadow-lg",
+    btnWhite:
+      "py-4 px-8 bg-white text-gray-600 border-white hover:border-gray-500 shadow-lg",
   };
 
   return (
