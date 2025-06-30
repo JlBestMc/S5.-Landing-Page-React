e03cc0c4731137a134e714eb73c940955dac6e71
JlBestMc <jlrcmt@gmail.com>
2025-06-30 03:01:10 +0200

    wip building and styling component Feature

--- src/components/atoms/tab.tsx
+++ src/components/atoms/tab.tsx
@@ -6,9 +6,9 @@ interface TabProps {
 
 export default function Tab({ content, onSelect, isSelected }: TabProps) {
   const selectedTab =
-    "tracking-wider text-3xl text-center py-10 px-10 w-full border-2 border-transparent border-b-red-400 cursor-[url('./assets/cursor.cur'),_auto]";
+    "tracking-wider text-lg text-center py-6 px-10 w-full border-4 border-transparent border-b-red-400 cursor-[url('./assets/cursor.cur'),_auto]";
   const unselectedTab =
-    "tracking-wider text-3xl text-center py-10 px-10 w-full border-2 border-transparent cursor-[url('./assets/cursor.cur'),_auto]";
+    "tracking-wider text-lg text-center py-6 px-10 w-full border-4 border-transparent cursor-[url('./assets/cursor.cur'),_auto]";
 
   return (
     <button
