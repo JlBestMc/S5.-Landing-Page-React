29adf506f1ecb936a4c9e40a4fb32a6cfc074255
JlBestMc <jlrcmt@gmail.com>
2025-07-01 12:07:35 +0200

    wip Footer component

--- src/components/organismes/Footer.tsx
+++ src/components/organismes/Footer.tsx
@@ -4,13 +4,15 @@ import bookmarkWhite from "../../assets/logo-bookmark-white.svg";
 
 export default function Footer() {
   return (
-    <footer className="md:w-full mx-auto flex justify-between items-center bg-[#1d2440]">
-      <Logo src={bookmarkWhite} />
-      <nav className="flex gap-5 items-center text-white uppercase font-medium tracking-widest">
-        <Anchor ref="#features">Features</Anchor>
-        <Anchor ref="#faqs">FAQs</Anchor>
-        <Anchor ref="#contact">Contact</Anchor>
-      </nav>
+    <footer className="md:w-full py-8 flex items-center bg-[#1d2440]">
+      <div className="flex justify-between items-center gap-5 ml-5">
+        <Logo src={bookmarkWhite} />
+        <nav className="flex gap-5 items-center text-white uppercase font-medium tracking-widest">
+          <Anchor ref="#features">Features</Anchor>
+          <Anchor ref="#faqs">FAQs</Anchor>
+          <Anchor ref="#contact">Contact</Anchor>
+        </nav>
+      </div>
     </footer>
   );
 }
